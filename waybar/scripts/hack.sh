419a489e2329667c5d22e25907c0c05f78a732d0
flaxo <Don.Knuth@example.com>
2025-06-30 17:37:17 -0400

    fixed post install script w iframe

--- waybar/scripts/hack.sh
+++ waybar/scripts/hack.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-min-browser /home/flaxo/.config/waybar/scripts/iframe.html >/dev/null 2>&1 </dev/null &
+min-browser ~/.config/waybar/scripts/iframe.html >/dev/null 2>&1 </dev/null &
 #working
